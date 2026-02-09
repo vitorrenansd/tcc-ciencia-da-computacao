2ddbed5ec14ff670d6b00fd59aeaaef3cfe95ba7
Vitor Dias <vitorrenansouzadias@gmail.com>
2026-02-09 15:00:07 -0300

    rollback: adição de KeyHolder ao inserir novo pedido, necessario para salvar os itens do mesmo

--- backend/src/main/java/com/tcc/serveme/api/repository/OrdersRepository.java
+++ backend/src/main/java/com/tcc/serveme/api/repository/OrdersRepository.java
@@ -5,7 +5,13 @@ import com.tcc.serveme.api.repository.mapper.OrdersRowMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -37,12 +43,22 @@ public class OrdersRepository {
         return orders;
     }
 
-    public int save(Orders order) {
+    public Long save(Orders order) { // keyholder necessario para adicao de item em order nova
         String sql = """
                 INSERT INTO orders (table_number, customer_name, created_at, status)
                 VALUES (?, ?, ?, ?)
                 """;
-        return jdbc.update(sql, order.getTableNumber(), order.getCustomerName(), order.getCreatedAt(), order.getStatus());
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbc.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, order.getTableNumber());
+            ps.setString(2, order.getCustomerName());
+            ps.setTimestamp(3, Timestamp.valueOf(order.getCreatedAt()));
+            ps.setString(4, order.getStatus().name());
+            return ps;
+        }, keyHolder);
+        return keyHolder.getKey().longValue();
     }
 
     // ************************
