87ce460271a8e13962377576bf6cf34d8e2e2061
Vitor Dias <vitorrenansouzadias@gmail.com>
2026-02-11 08:28:40 -0300

    update: H2 DB marcado como runtimeOnly no build.gradle

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
-	implementation("com.h2database:h2:2.4.240")
+	runtimeOnly("com.h2database:h2:2.4.240")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
