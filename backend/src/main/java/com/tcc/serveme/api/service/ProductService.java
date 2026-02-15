fb0ab34c1b2f2f2cd4477e3c54c0bbd64cca2463
Vitor Dias <vitorrenansouzadias@gmail.com>
2026-02-15 16:43:44 -0300

    new: criação da service layer de Product

---
+++ backend/src/main/java/com/tcc/serveme/api/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.tcc.serveme.api.service;
+
+import com.tcc.serveme.api.repository.ProductRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepo;
+
+    @Autowired
+    public ProductService(ProductRepository productRepo) {
+        this.productRepo = productRepo;
+    }
+
+}
