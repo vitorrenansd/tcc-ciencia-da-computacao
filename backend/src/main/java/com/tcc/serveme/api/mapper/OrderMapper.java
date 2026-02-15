e227298f091ee6781d915b80671648b8d7846f6f
Vitor Dias <vitorrenansouzadias@gmail.com>
2026-02-14 23:51:22 -0300

    OrderMapper agora fica em um package proprio

--- backend/src/main/java/com/tcc/serveme/api/mapper/OrderMapper.java
+++ backend/src/main/java/com/tcc/serveme/api/mapper/OrderMapper.java
@@ -1,7 +1,8 @@
-package com.tcc.serveme.api.service;
+package com.tcc.serveme.api.mapper;
 
 import com.tcc.serveme.api.dto.OrderRequest;
 import com.tcc.serveme.api.dto.OrderItemRequest;
+import com.tcc.serveme.api.dto.OrderResponse;
 import com.tcc.serveme.api.model.Orders;
 import com.tcc.serveme.api.model.OrderItem;
 
@@ -13,11 +14,22 @@ public class OrderMapper {
         );
     }
 
-    public static OrderItem toModel(OrderItemRequest dto) {
+    public static OrderItem toModel(OrderItemRequest dto, Long orderId) {
         return new OrderItem(
+            orderId,
             dto.productId(),
             dto.quantity(),
             dto.notes()
+        );
+    }
+
+    public static OrderResponse toResponse(Orders order) {
+        return new OrderResponse(
+                order.getId(),
+                order.getTableNumber(),
+                order.getCustomerName(),
+                order.getCreatedAt(),
+                order.getStatus().name()
         );
     }
 }
