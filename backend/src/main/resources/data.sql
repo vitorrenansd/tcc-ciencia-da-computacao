59cc4f86b68e3c338acee64acdb0e71b2b6c33ec
Vitor Dias <vitorrenansouzadias@gmail.com>
2026-02-18 21:21:05 -0300

    test: adicionados 2 novos produtos base

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO product_category (name, inactive) VALUES
 ('GERAL', FALSE);
+
+INSERT INTO product (category_id, name, description, price, inactive) VALUES
+('1', 'Fritas media', 'Batata frita no oleo com sal e pimenta', 21.99, FALSE),
+('1', 'Bife de picanha', 'Picanha feita na brasa, escolha o ponto por favor', 39.99, FALSE);
